527dad09328b93a9a7e2d52e6e16f34cda5cf4f4
Zuvean <hmjohnson915@gmail.com>
2020-08-02 15:32:11 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='zkill_database_git',
-    version='testing1.0',
-    packages=['.gitattritubtes',],
+    name='Trial Run',
+    version='V1.1',
+    packages=['zkill_database_git',],
 )
